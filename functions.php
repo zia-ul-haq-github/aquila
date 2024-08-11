b71b1e3f54b279f4072e1f194ad06c69cce0d292
mairajulhaq <ziaulhaq.fed@gmail.com>
2024-08-11 14:41:43 +0500

    zia -video 06 -commit - include stylesheet dynamic

--- functions.php
+++ functions.php
@@ -4,3 +4,12 @@
  * 
  * @package Aquila
  */
+
+//  print_r(filemtime(get_template_directory() . '/style.css'));
+ 
+ function aquila_enqueue_scripts() {
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+ }
+
+ add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
+ ?>

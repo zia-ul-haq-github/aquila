ccad3d84f2bdbbf0f92b0fc20dee754a64a1641b
mairajulhaq <ziaulhaq.fed@gmail.com>
2024-08-18 00:49:49 +0500

    zia-v19 -commit- add custom logo & show on front

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WordPress Theme Development</title>
     <?php wp_head(); ?>
 </head>
 <body>
--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -26,7 +26,15 @@ class AQUILA_THEME {
     }
 
     public function setup_theme() {
+
         add_theme_support( 'title-tag' );
+        add_theme_support( 'custom-logo', [
+            'header-text'          => ['site-title', 'site-description'],
+            'height'               => 100,
+		    'width'                => 400,
+		    'flex-height'          => true,
+		    'flex-width'           => true,
+        ] );
     }
 
 }

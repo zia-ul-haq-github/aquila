2d9d52c0774c6f5b339ad419928c9b0f4195a3bb
mairajulhaq <ziaulhaq.fed@gmail.com>
2024-08-26 06:08:40 +0500

    v30 -commit - Display Posts in bootstrap Grid System

--- index.php
+++ index.php
@@ -17,27 +17,57 @@
             ?>
             <div class="container">
                <?php
-               /**
-                * Display the page title using single_post_title()function
-                * first check the condiction is it home page and not a front page and call a function
-                */
-                  if( is_home() && !is_front_page()) {
+
+                  /**
+                   * Display the page title using single_post_title()function
+                  * first check the condiction is it home page and not a front page and call a function
+                  */
+                  if( is_home() && ! is_front_page() ) {
                      ?>
-                        <header class="mb-5">
-                           <h1 class="page-title screen-render-text">
-                              <?php single_post_title(); ?>
-                           </h1>
-                        </header>
+                     <header class="mb-5">
+                        <h1 class="page-title screen-render-text">
+                           <?php single_post_title(); ?>
+                        </h1>
+                     </header>
                      <?php
                   }
                ?>
-               <?php
-               while( have_posts() ) : the_post();
-                  the_title();
-                  the_content();
-                  the_post_thumbnail();
-               endwhile;
-               ?>
+                    
+               <!-- Display The Posts in Bootstrap Grid System    -->
+               <div class="row">
+                  <?php
+                        $index = 0;
+                        $no_of_columns = 3;
+
+                        // Start the loop
+                        while( have_posts() ) : the_post();
+                           
+                        /** First check the condiction and devied the both variables and their value is not equll to zero then create a grids and 
+                        * call the other  funcions then increment the index variable 
+                        */
+                           if( 0 === $index % $no_of_columns) {
+                              ?>
+                              <div class="col-lg-4 col-md-6 col-sm-12">
+                              <?php
+                           }
+
+                           ?>
+                              <h3> <?php the_title(); ?> </h3>
+                              <div> <?php the_excerpt(); ?></div>
+                           <?php
+                           
+                           $index ++;
+
+                           if( 0 !== $index && 0 === $index % $no_of_columns) {
+                              ?>
+                              </div>
+                              <?php
+                           }
+               
+                     endwhile;
+                  ?>
+               </div>
+                  
             </div>
             <?php
          }

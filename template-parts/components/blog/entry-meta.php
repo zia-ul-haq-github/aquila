21232431aaaaf89357415dc8804c8f05fad11044
mairajulhaq <ziaulhaq.fed@gmail.com>
2024-08-30 03:58:09 +0500

    v39 -commit - Display Posted on Data & Time on Blog posts

---
+++ template-parts/components/blog/entry-meta.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template for the entry meta means metadata.
+ * 
+ * @package Aquila
+ */
+?>
+
+<!-- inside of dive we call the function to display the post created and modifiy time and date -->
+<div class="entry-meta mb-3">
+    <?php aquila_posted_on(); ?>
+</div>

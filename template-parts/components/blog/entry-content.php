9ebe0f6d3bcf9967aa563b0c5e68bcb88f095e1d
mairajulhaq <ziaulhaq.fed@gmail.com>
2024-09-04 23:44:11 +0500

    v44 -commit - Add pagination on single post

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -31,6 +31,15 @@
                     the_title('<span class= "screen-reader-text">"','"</span>', false)
                 )
             );
+
+            // Add Pagination on single post
+            wp_link_pages(
+                [
+                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'aquila'),
+                    'after' => '</div>',
+                ]
+            );
+
         } else{
             aquila_the_excerpt(200);   // Call the function
             echo aquila_excerpt_more(); // echo function
